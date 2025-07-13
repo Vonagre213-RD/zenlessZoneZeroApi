a7474e90f0098b9675bc94d3b9cc7c3d0d6c65f4
Vonagre213-RD <silveriodenzel@gmail.com>
2025-07-12 20:44:52 -0400

    Cors agregado para que se puedan hacer peticiones

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 import express from 'express'
+import cors from 'cors'
 import * as rt from './Functions/routes.js'
 
 const app = express()
+app.use(cors())
 rt.Router(app)
 
 app.listen(3000, (err) => {
